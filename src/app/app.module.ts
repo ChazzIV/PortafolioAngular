18076ac8f7292e803700fe257afdd1382ff91d4e
ChazzIV <chazz.iv@gmail.com>
2020-09-09 13:19:23 -0500

    Seccion 3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,17 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './shared/header/header.component';
 import { FooterComponent } from './shared/footer/footer.component';
 import { PortafolioComponent } from './pages/portafolio/portafolio.component';
+import { AboutComponent } from './pages/about/about.component';
+import { ItemComponent } from './pages/item/item.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    PortafolioComponent
+    PortafolioComponent,
+    AboutComponent,
+    ItemComponent
   ],
   imports: [
     BrowserModule,
@@ -21,4 +25,5 @@ import { PortafolioComponent } from './pages/portafolio/portafolio.component';
   providers: [],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
