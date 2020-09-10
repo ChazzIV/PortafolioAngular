f343df0af07ca1d3a9fe3ae06e7388b796cf2815
ChazzIV <chazz.iv@gmail.com>
2020-09-09 20:43:58 -0500

    fin de la seccion

--- src/app/services/info-pagina.service.ts
+++ src/app/services/info-pagina.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { InfoPagina } from '../interfaces/info-pagina-interfaces';
+import { EquipoPagina } from '../interfaces/equipo-pagina-interfaces';
 
 @Injectable({
   providedIn: 'root'
@@ -8,19 +9,39 @@ import { InfoPagina } from '../interfaces/info-pagina-interfaces';
 export class InfoPaginaService {
 
   info: InfoPagina = {};
+
+  equipo: EquipoPagina = {};
+
   cargada = false;
 
   constructor( private http: HttpClient) {
     console.log('Servicio de pagina listo');
 
-    // leer el archivo json
+    this.cargarInfo();
+
+    this.cargarEquipo();
+
+   }
+
+   private cargarInfo(): void {
+         // leer el archivo json
     this.http.get('assets/data/data-pagina.json') // no funciona si no se utiliza un suscribe
         .subscribe( (resp: InfoPagina) => {
-
           this.cargada = true;
           this.info = resp;
-          console.log(resp);
+         // console.log(resp);
 
         });
    }
+
+   private cargarEquipo(): void {
+    this.http.get('https://angular-html-9adfb.firebaseio.com/equipo.json')
+        .subscribe( ( resp: EquipoPagina) => {
+          this.cargada = true;
+          this.equipo = resp;
+          // console.log(resp);
+        });
+   }
+
+
 }
